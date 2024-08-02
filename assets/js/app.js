acab720ea95df3fdf0d4692ac696cd7d4ea27944
Panesur <surajneupane02@gmail.com>
2024-08-02 19:56:31 +1000

    added modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,6 @@
-import { ACCESS_KEY } from './constants.js';
-
-const labels = ['Montréal', 'Canada', 'Collective', 'Table', 'Interior', 'Chair', 'Coffee Shop', 'Coffee Machine', 'Coffee', 'Cafe'];
-
-function populateLabels() {
-    const labelElements = labels.map(label => {
-        return `<button class="label-btn">${label}</button>`;
-    });
-    document.querySelector('.labels-wrap').innerHTML = labelElements.join("");
-    document.querySelectorAll('.label-btn').forEach(button => {
-        button.addEventListener('click', async (event) => {
-            const query = event.target.textContent;
-            await searchImages(query);
-        })
-    })
-}
+import searchImages from './search.js';
+import populateLabels from './labels.js';
+import {getCookie} from './cookies.js';
 
 populateLabels();
 
@@ -24,32 +11,9 @@ document.querySelector('form').addEventListener('submit', async function(event) 
     await searchImages(query);
 });
 
-async function  searchImages(query) {
-    const now = new Date();
-    const expires =  now.setTime(now.getTime() + (24 * 60 * 60 * 1000));
-
-    document.cookie = `query=${query};expires=${new Date(expires)};path=/`;
-    console.log({query});
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${ACCESS_KEY}`;
-    console.log({url})
-
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    const images = data.results;
-    let imageElements = '';
-    for(let image of images) {
-        const imageUrl = image.urls.regular;
-        imageElements += `<img src="${imageUrl}" alt="">`; 
-        console.log({imageUrl});
-    }
-    document.querySelector('.images').innerHTML = imageElements;
-}
 
 
 (async () => {
-    const cookie = document.cookie;
-    const query = cookie.split("=")[1];
-    console.log(query);
+    const query = getCookie();
     await searchImages(query);
 })()
---
+++ assets/js/constant.js
@@ -0,0 +1,2 @@
+export const ACCESS_KEY = "SonkqFe1zfl2gZFKkoNvkB_83GgTDVrMKLm0fT7ZYTo";
+export const SECRET_KEY = "qC-SbmpJmAOiURSf7D4qWP8KaLbyu5aL2ligwoGffjw";
